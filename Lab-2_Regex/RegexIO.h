1f543115d4531284bd586ee1235b922d5784f88d
vurufugangu <vurufugangu@gmail.com>
2015-05-04 15:46:41 -0700

    Implemented RegexIO member functions stringMatch, searchStringForMatch, and findMatchInFile

--- Lab-2_Regex/RegexIO.h
+++ Lab-2_Regex/RegexIO.h
@@ -29,8 +29,8 @@ public:
 	// setters
 	void setPattern(const std::string &patt);
 	// other methods
-	bool stringMatch(const std::string &text);
-	bool searchStringForMatch(const std::string &text);
+	bool stringMatch(const std::string &text) { return std::regex_match(text, rex); }
+	bool searchStringForMatch(const std::string &text) { return std::regex_search(text, rex); }
 	bool searchFileForMatch(const std::string &filename);
 	std::string findMatchInFile(const std::string &filename, unsigned int matchNum);
 };
@@ -45,6 +45,53 @@ RegexIO::RegexIO(const std::string &patt)
 {
 	pattern = patt;
 	rex = regex(pattern);
+}
+
+void RegexIO::setPattern(const std::string &patt)
+{
+	pattern = patt;
+	rex = regex(pattern);
+}
+
+bool RegexIO::searchFileForMatch(const std::string &filename)
+{
+	std::ifstream ifs;
+	
+	ifs.open(filename);
+	if (!ifs.is_open()) // if file was not opened
+		return false;
+
+	std::string line;
+	while (getline(ifs, line))
+	{
+		if (std::regex_search(line, rex))
+			return true;
+	}
+}
+
+std::string RegexIO::findMatchInFile(const std::string &filename, unsigned int matchNum)
+{
+	std::ifstream ifs;
+
+	ifs.open(filename);
+	if (!ifs.is_open() || matchNum == 0)
+		return "";
+
+	std::string line;
+	std::string match;
+	unsigned int matchCount = 0;
+	while (getline(ifs, line) && matchCount != matchNum)
+	{
+		if (std::regex_search(line, rex))
+		{
+			match = line;
+			matchCount++;
+		}
+	}
+
+	// if the match number wanted is greater than the total number of matches
+	// return an empty string, otherwise return the nth match (n = matchNum)
+	return (matchCount != matchNum ? "" : match);
 }
 
 #endif // REGEX_IO_H_
