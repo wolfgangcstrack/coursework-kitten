590684f4a5839fd28eeb24e3e278537755bd6be6
vurufugangu <vurufugangu@gmail.com>
2015-05-11 21:16:11 -0700

    Refactored OptionSet to use ArrayList of Options, added get/set Option choice methods

--- Assignment-3/src/model/OptionSet.java
+++ Assignment-3/src/model/OptionSet.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 
 class OptionSet implements java.io.Serializable {
 	// serialVersionUID generated with serialver
-	private static final long serialVersionUID = 5537926375200316701L;
+	// private static final long serialVersionUID = 5537926375200316701L;
 	private String name;
 	private ArrayList<Option> options;
 	private Option choice;
@@ -31,10 +31,9 @@ class OptionSet implements java.io.Serializable {
 	}
 	
 	// getters -----------------------------------------------------------
-	/*
 	protected String getName() { return name; }
 	
-	protected Option[] getOptions() { return options; }
+	protected Option[] getOptions() { return ((Option[])options.toArray()); }
 	
 	protected Option[] getOptions(int length) { return getOptions(0, length); }
 	
@@ -45,53 +44,49 @@ class OptionSet implements java.io.Serializable {
 	}
 	
 	protected Option getOption(int index) {
-		if (index >= 0 && index < options.length)
-			return options[index];
+		if (index >= 0 && index < options.size())
+			return options.get(index);
 		return null; // when index is not within range
 	}
-	*/
+	
+	protected Option getOptionChoice() { return choice; }
+	
 	// setters -----------------------------------------------------------
-	/*
 	protected void setName(String name) { this.name = name; }
 	
-	protected void setOptions(Option options[]) { this.options = options; }
+	//protected void setOptions(Option options[]) { this.options = options; }
 	
-	protected void setOptionName(int index, String opName) { options[index].setName(opName); }
+	protected void setOptionName(int index, String opName) { options.get(index).setName(opName); }
 	
-	protected void setOptionPrice(int index, float opPrice) { options[index].setPrice(opPrice); }
+	protected void setOptionPrice(int index, float opPrice) { options.get(index).setPrice(opPrice); }
 	
 	protected void setOption(int index, String opName, float opPrice) {
-		options[index] = new Option(opName, opPrice);
-	}
-	*/
-	// add Option methods ------------------------------------------------
-	/*
-	protected boolean addOption(String opName, float opPrice) {
-		// function does not return false (no parameter checking) for now
-		
-		Option newoptions[] = new Option[options.length+1];
-		// copy old options to the new array
-		for (int i = 0; i < options.length; i++)
-			newoptions[i] = options[i];
-		// add the new Option
-		newoptions[options.length] = new Option(opName, opPrice);
-		
-		options = newoptions; // copy the new array to this.options
-		return true;
+		options.set(index, new Option(opName, opPrice));
 	}
 	
-	// delete functions -------------------------
-	protected boolean deleteOption(int index) {
-		if (index < 0 || index >= options.length)
-			return false;
-		
-		Option temp[] = new Option[options.length-1];
-		for (int i = 0; i < temp.length; i++) {
-			if (i < index)
-				temp[i] = options[i];
-			else if (i > index)
-				temp[i] = options[i+1];
+	protected void setOptionChoice(String optionName) {
+		for (Option option : options) {
+			if (option.getName().equals(optionName))
+				choice = option;
 		}
+	}
+	
+	protected void setOptionChoice(int optionIndex) {
+		if (optionIndex >= 0 && optionIndex <= options.size() - 1)
+			choice = options.get(optionIndex);
+	}
+	
+	// add Option methods ------------------------------------------------
+	protected boolean addOption(String opName, float opPrice) {
+		return options.add(new Option(opName, opPrice));
+	}
+	
+	// delete Option methods ---------------------------------------------
+	protected boolean deleteOption(int index) {
+		if (index < 0 || index >= options.size())
+			return false;
+		// else
+		options.remove(index);
 		return true;
 	}
 	
@@ -99,11 +94,10 @@ class OptionSet implements java.io.Serializable {
 	public String toString() {
 		StringBuilder sb = new StringBuilder(name);
 		String newline = "\n", tab = "\t";
-		for (int i = 0; i < options.length; i ++)
+		for (int i = 0; i < options.size(); i++)
 			sb.append(newline)
-			.append(tab).append(tab).append(options[i].toString());
+			.append(tab).append(tab).append(options.get(i).toString());
 		
 		return sb.toString();
 	}
-	*/
 }
