83a6bfcff99da9b34c64a5be14f5e6ecb0c39e7b
vurufugangu <vurufugangu@gmail.com>
2015-05-13 15:21:16 -0700

    Outlined function layout in main.cpp

--- Lab-3_Threading/main.cpp
+++ Lab-3_Threading/main.cpp
@@ -8,14 +8,35 @@ This is the main application for this lab.
 
 #include "XmlNodeList.h"
 #include "XmlRegexIO.h"
-#include "XmlRobot.h"
+#include "XmlRobot.h" // XmlRobot class extends the given Robot class for this lab
 #include "ConsoleColor.h"
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 using namespace std;
 
+// the following methods are called from main()
+void openDataFile(ifstream &ifs, const string &filename);
+void readTagsFromFile(const ifstream &ifs, vector<string> &tags);
+void instantiateRobotCommands(const vector<string> &tags, vector<unique_ptr<XmlNode>> &robotCommands);
+void executeRobotCommands(const vector<unique_ptr<XmlNode>> &robotCommands);
+
+// the following methods are called from executeRobotCommands()
+void useRobotA1(const XmlRobot &robot); // use thread 1
+void useRobotA2(const XmlRobot &robot); // use thread 2
+void useRobotA3(const XmlRobot &robot); // use thread 3
+void useRobotA4(const XmlRobot &robot); // use thread 4
+
 int main()
 {
+	string filename = "RobotData.xml";
+	vector<string> tags;
+	vector<unique_ptr<XmlNode>> robotCommands;
+	ifstream ifs;
 
+	openDataFile(ifs, filename);
+	readTagsFromFile(ifs, tags);
+	instantiateRobotCommands(tags, robotCommands);
+	executeRobotCommands(robotCommands); // calls the useRobot methods
 }
