3044104b3cdae5ab7abdd1e57b887a89f85103df
vurufugangu <vurufugangu@gmail.com>
2015-05-23 17:23:42 -0700

    Added Fleet class for implementation as a singleton class that holds the LinkedHashMap of automobiles in ProxyAuto; changed comment header in all files to not include OS because THIS IS JAVA FOR CRYIN OUT LOUD

--- Assignment-4/src/adapter/BuildAuto.java
+++ Assignment-4/src/adapter/BuildAuto.java
@@ -1,7 +1,7 @@
 /*
  * CIS 35B
  * Author: Wolfgang C. Strack
- * OS/Compiler: Windows 8/Java 8 with Eclipse
+ * Compiler: Java 8 with Eclipse
  * 
  * This is the BuildAuto class.
  */
---
+++ Assignment-4/src/model/Fleet.java
@@ -0,0 +1,12 @@
+/*
+ * CIS 35B
+ * Author: Wolfgang C. Strack
+ * Compiler: Java 8 with Eclipse
+ * 
+ * This class represents the Model/Automotive class.
+ */
+package model;
+
+public class Fleet {
+
+}
