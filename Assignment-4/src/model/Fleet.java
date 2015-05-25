349d78c623b1c59a07b7ae2941f11f01b1db109a
vurufugangu <vurufugangu@gmail.com>
2015-05-24 22:25:29 -0700

    Implemented model.Fleet Read and Update methods

--- Assignment-4/src/model/Fleet.java
+++ Assignment-4/src/model/Fleet.java
@@ -12,23 +12,28 @@ import java.util.Map;
 import java.util.LinkedHashMap;
 
 public class Fleet {
-	private static final Fleet fleet = new Fleet();
-	private static Map<String, Automobile> automobiles;
+	private static final Fleet instance = new Fleet();
+	private static final Object instanceLock = new Object();
+	private static Map<String, Automobile> fleet; // is thread-safe
 	
 	/** private constructor ensures this Singleton Fleet will only be
 	 * instantiated once; that one time it is instantiated is here */
 	private Fleet() {
-		// instantiate a synchronized LinkedHashMap
-		automobiles = Collections.synchronizedMap(new LinkedHashMap<String, Automobile>());
+		// instantiate a thread-safe LinkedHashMap
+		fleet = Collections.synchronizedMap(new LinkedHashMap<String, Automobile>());
 	}
 	
 	// method for getting the singleton Fleet instance -------------------
-	public synchronized static Fleet getInstance() { return fleet; }
+	public static Fleet getInstance() {
+		synchronized (instanceLock) {
+			return instance;
+		}
+	}
 	
 	// private helper methods ------------------------------------------
-	private Automobile getAutoWithKey(String autoMake, String autoModel) {
-		return automobiles.get(getKey(autoMake, autoModel));
-	}
+//	private Automobile getAutoWithKey(String autoMake, String autoModel) {
+//		return fleet.get(getKey(autoMake, autoModel));
+//	}
 	
 	private String getKey(String autoMake, String autoModel) {
 		StringBuilder key = new StringBuilder(autoMake);
@@ -37,13 +42,13 @@ public class Fleet {
 	}
 	
 	// Create methods ----------------------------------------------------
-	public synchronized boolean addAutomobile(Automobile auto) {
+	public boolean addAutomobile(Automobile auto) {
 		String key = getKey(auto.getMake(), auto.getModel());
 		
-		if (automobiles.containsKey(key)) {
+		if (fleet.containsKey(key)) {
 			return false;
 		} else {
-			automobiles.put(key, auto);
+			fleet.put(key, auto);
 			return true;
 		}
 	}
@@ -54,12 +59,10 @@ public class Fleet {
 			String newOptionSetName) {
 		String key = getKey(autoMake, autoModel);
 		
-		synchronized (automobiles) {
-			if (automobiles.containsKey(key)) {
-				return automobiles.get(key).addOptionSet(newOptionSetName);
-			} // else
-			return false;
-		}
+		if (fleet.containsKey(key)) {
+			return fleet.get(key).addOptionSet(newOptionSetName);
+		} // else
+		return false;
 	}
 	
 	public boolean addOption(
@@ -70,16 +73,88 @@ public class Fleet {
 			float newOptionPrice) {
 		String key = getKey(autoMake, autoModel);
 		
-		synchronized (automobiles) {
-			if (automobiles.containsKey(key)) {
-				return automobiles.get(key).addOption(optionSetName, newOptionName, newOptionPrice);
-			} // else
-			return false;
-		}
+		if (fleet.containsKey(key)) {
+			return fleet.get(key).addOption(optionSetName, newOptionName, newOptionPrice);
+		} // else
+		return false;
 	}
 	
 	// Read methods ------------------------------------------------------
-//	public synchronized Automobile getAuto(String autoMake, String autoModel) {
-//		return this.getAutoWithKey(autoMake, autoModel);
-//	}
+	public Automobile getAuto(String autoMake, String autoModel) {
+		return fleet.get(getKey(autoMake, autoModel));
+	}
+	
+	// Update methods ----------------------------------------------------
+	public boolean updateOptionSetName(
+			String autoMake,
+			String autoModel,
+			String optionSetName,
+			String newName
+			) {
+		String key = getKey(autoMake, autoModel);
+		
+		if (fleet.containsKey(key)) {
+			return fleet.get(key).updateOptionSetName(optionSetName, newName);
+		} // else
+		return false;
+	}
+	
+	public boolean updateOptionName(
+			String autoMake,
+			String autoModel,
+			String optionSetName,
+			String optionName,
+			String newName
+			) {
+		String key = getKey(autoMake, autoModel);
+		
+		if (fleet.containsKey(key)) {
+			return fleet.get(key).updateOptionName(optionSetName, optionName, newName);
+		} // else
+		return false;
+	}
+	
+	public boolean updateOptionPrice(
+			String autoMake,
+			String autoModel,
+			String optionSetName,
+			String optionName,
+			float newPrice
+			) {
+		String key = getKey(autoMake, autoModel);
+		
+		if (fleet.containsKey(key)) {
+			return fleet.get(key).updateOptionPrice(optionSetName, optionName, newPrice);
+		} // else
+		return false;
+	}
+	
+	public boolean updateOption(
+			String autoMake,
+			String autoModel,
+			String optionSetName,
+			String optionName,
+			String newName,
+			float newPrice) {
+		String key = getKey(autoMake, autoModel);
+		
+		if (fleet.containsKey(key)) {
+			return fleet.get(key).updateOption(optionSetName, optionName, newName, newPrice);
+		} // else
+		return false;
+	}
+	
+	public boolean updateOptionChoice(
+			String autoMake,
+			String autoModel,
+			String optionSetName,
+			String optionName
+			) {
+		String key = getKey(autoMake, autoModel);
+		
+		if (fleet.containsKey(key)) {
+			return fleet.get(key).updateOptionChoice(optionSetName, optionName);
+		} // else
+		return false;
+	}
 }
