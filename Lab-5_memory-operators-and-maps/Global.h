08d2a0442a0bccf7c702be35d5817b5cb795f893
vurufugangu <vurufugangu@gmail.com>
2015-05-31 23:08:07 -0700

    Added Global declarations for dynamic memory operator overloads

---
+++ Lab-5_memory-operators-and-maps/Global.h
@@ -0,0 +1,18 @@
+/*
+Lab 5 - Overloading dynamic memory operators and using STL maps
+Wolfgang C. Strack
+Windows 8 Visual C++ 2013
+
+This header file has global project definitions for the dynamic memory
+operator overloads.
+*/
+
+#ifndef GLOBAL_DEFS_H_
+#define GLOBAL_DEFS_H_
+
+void * operator new(size_t);
+void * operator new[](size_t);
+void operator delete(void *);
+void operator delete[](void *);
+
+#endif // GLOBAL_DEFS_H_
