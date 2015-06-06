824d63f67848a47644ee2eb38ef0b9d9dcb12060
vurufugangu <vurufugangu@gmail.com>
2015-06-05 23:57:24 -0700

    smd main

--- Lab-5_memory-operators-and-maps/main.cpp
+++ Lab-5_memory-operators-and-maps/main.cpp
@@ -17,29 +17,5 @@ using namespace std;
 
 int main()
 {
-	Falsegrind::startFalsegrind();
-	cout << "Initializing vector...\n\n";
-	vector<pair<long, long>> *randomLongs = new vector<pair<long, long>>();
-	Falsegrind::printAllocationCount(cout);
-
-	cout << "Seeding random number generator...\n\n";
-	srand(time(0));
-	Falsegrind::printAllocationCount(cout);
-
-	cout << "Inserting 100 pair pointers of random longs to the vector...\n\n";
-	for (int i = 0; i < 100; i++)
-	{
-		pair<long, long> randPair(rand() % aReallyBigNumber + 1, rand() % aReallyBigNumber + 1);
-		cout << "Inserting " << randPair.first << " and " << randPair.second << endl;
-		randomLongs->push_back(randPair);
-		Falsegrind::printAllocationCount(cout);
-	}
-
-	Falsegrind::printAllocationCount(cout);
-
-	randomLongs->clear();
-	delete randomLongs;
-	Falsegrind::printAllocationCount(cout);
-	Falsegrind::closeFalsegrind();
 	return 0;
 }
