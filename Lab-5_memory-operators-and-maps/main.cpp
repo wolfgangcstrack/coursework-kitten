fb0aa28d99f83f125120393bf74008b2772d18ed
vurufugangu <vurufugangu@gmail.com>
2015-06-07 00:13:56 -0700

    implemented sUPER-cOMPLEX method for use of new in main smd

--- Lab-5_memory-operators-and-maps/main.cpp
+++ Lab-5_memory-operators-and-maps/main.cpp
@@ -13,30 +13,48 @@ This file contains the main application for this lab.
 #include <list>
 using namespace std;
 
-#define aNumber int(1 << 5)
-#define aBigNumber int(1 << 10)
+#define aNumber int(1 << 3)
+#define aBigNumber int(1 << 5)
 #define aReallyBigNumber long(1 << 20)
+#define RANDOM_LONG rand() % aReallyBigNumber + 1
 
-//void useNewAtLeastTenTimes(list<list<pair<long, long>*>*> &reallyLongList);
-//void useDeleteAtLeastTenTimes(list<list<pair<long, long>*>*> &reallyLongList);
+void useNewAtLeastTenTimes(list <pair<long, long>**> &reallyLongList);
+void useDeleteAtLeastTenTimes(list <pair<long, long>**> &reallyLongList);
 
 int main()
 {
-	//list <list<pair<long, long>*>*> reallyLongList;
+	list <pair<long, long>**> reallyLongList;
 
-	//Falsegrind::startFalsegrind();            // start memory monitor application
+	Falsegrind::startFalsegrind();            // start memory monitor application
 
-	//useNewAtLeastTenTimes(reallyLongList);
-	//useDeleteAtLeastTenTimes(reallyLongList);
+	useNewAtLeastTenTimes(reallyLongList);
+	useDeleteAtLeastTenTimes(reallyLongList);
 
-	//Falsegrind::closeFalsegrind();            // close memory monitor application
+	Falsegrind::closeFalsegrind();            // close memory monitor application
 
-	//return 0;
+	return 0;
 }
 
-void useNewAtLeastTenTimes(list<list<pair<long, long>>> &reallyLongList)
+void useNewAtLeastTenTimes(list<pair<long, long>**> &reallyLongList)
 {
-	/*Falsegrind::printAllocationCount(cout);
-	cout << "Inserting " << aNumber << " list pointers of " << aBigNumber << " random pair<long, long> pointers each.\n\n"
-	for (int i = 0; i < )*/
+	cout << "Total number of dynamic memory allocations: " << Falsegrind::getTotalAllocationCount() << "\n\n";
+
+	srand(time(0));
+
+	cout << "Inserting " << aNumber << " pair arrays of " << aBigNumber << " random pair<long, long> pointers each.\n\n";
+	for (int i = 0; i < aNumber; i++)
+	{
+		cout << "Creating new pair array... ";
+		pair<long, long> **newPair = new pair<long, long>*[aBigNumber]; // uses operator new[]
+		cout << "Total number of dynamic memory allocations: " << Falsegrind::getTotalAllocationCount() << endl;
+		for (int j = 0; j < aBigNumber; j++)
+		{
+			long rlong1, rlong2;
+			cout << "Creating new random pair<long, long> pointer that holds " << (rlong1 = RANDOM_LONG) << " and " << (rlong2 = RANDOM_LONG);
+			newPair[j] = new pair<long, long>(rlong1, rlong2);
+			cout << "... Total dynamic memory allocations: " << Falsegrind::getTotalAllocationCount() << endl;
+		}
+		reallyLongList.push_back(newPair);
+		cout << endl;
+	}
 }
