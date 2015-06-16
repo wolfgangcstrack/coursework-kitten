40672610a96d12cde5e43db37dd624a54b5b2fd3
vurufugangu <vurufugangu@gmail.com>
2015-06-15 17:53:29 -0700

    Started client package implementation

---
+++ Assignment-5/src/client/CarModelOptionsIO.java
@@ -0,0 +1,12 @@
+/*
+ * CIS 35B
+ * Author: Wolfgang C. Strack
+ * Compiler: Java 8 with Eclipse
+ * 
+ * This is the CarModelOptionsIO class.
+ */
+package client;
+
+public class CarModelOptionsIO {
+
+}
---
+++ Assignment-5/src/client/SelectCarOption.java
@@ -0,0 +1,12 @@
+/*
+ * CIS 35B
+ * Author: Wolfgang C. Strack
+ * Compiler: Java 8 with Eclipse
+ * 
+ * This is the SelectCarOption class.
+ */
+package client;
+
+public class SelectCarOption {
+
+}
