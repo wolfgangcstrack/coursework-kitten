2b5ba5d3b627e71f571e4f2cc454f5b00cb16d0d
Wolfgang C. Strack <vurufugangu@gmail.com>
2015-07-05 16:18:52 -0700

    New merge script ready for testing

--- merge_git_repo.bash
+++ merge_git_repo.bash
@@ -1,80 +1,29 @@
 #!/bin/bash
 # Author: Wolfgang C. Strack
-# This script takes arguments for a repository to merge to the current repository that this script
-# is in. I decided to automate this task since I do it frequently when merging other repositories
-# to coursework-kitten, which is intended to hold all git repositories of code from the courses I
-# have taken in the past.
+# This script merges a git repository as a subdirectory of the current git
+# repository that the script is in
 
 ##### Initialize some stuff ##############################################
 usageMessage="Usage: $0 repo_name repo_remote_path"
-hideDir=".HIDE_ALL_FILES"
 
 ##### Functions ##########################################################
-checkArgs () # takes the $@ variable as an argument
-{
-	if [ $# -ne 2 ]; then
-		echo $usageMessage
-		exit 1
-	fi
-}
-
-hideFiles ()
-{
-	mkdir $hideDir
-	
-	for file in $(ls -a); do
-		if [ "$file" != ".git" -a "$file" != "$hideDir" -a "$file" != "." -a "$file" != ".." ]; then
-			mv -t $hideDir/ $file # &> /dev/null
-		fi
-	done
-}
-
-unhideFiles ()
-{
-	# mv -t ./ $hideDir/* $hideDir/.* # &> /dev/null
-	for file in $(ls -a $hideDir); do
-		if [ "$file" != "." -a "$file" != ".." ]; then
-			mv -t ./ $hideDir/$file # &>/dev/null
-		fi
-	done
-
-	rmdir $hideDir
-	
-	echo;echo;echo
-
-	ls -alF --color=tty
-}
-
 mergeRepo ()
 {
-	git remote add $1 $2
-	git fetch $1
-	git merge -m "Merge remote-tracking branch '$1/master'" $1/master
-}
-
-moveRepoToDir ()
-{
-	mkdir $1
-	
-	for file in $(ls -a); do
-		if [ "$file" != ".git" -a "$file" != "$1" -a "$file" != "$hideDir" -a "$file" != "." -a "$file" != ".." ]; then
-			mv -t $1/ $file # &> /dev/null
-		fi
-	done
-}
-
-pushAllChanges ()
-{
-	git add -A
-	git commit -am "Merged $1"
-	echo;echo
-	git push
+	git remote add -f $1 $2
+	git merge -s ours --no-commit $1/master
+	git read-tree --prefix=$1/ -u $1/master
+	git commit -m "Merge git repo '$1' into subdirectory '$1/'"
 }
 
 ##### Main ###############################################################
-checkArgs $@
-hideFiles # hides files in current directory to ensure no files are overwritten
+if [ $# -ne 2 ]; then # check number of args
+	echo $usageMessage
+	exit 1
+fi
+
+if [ ! -d '.git' ]; then # check if current dir is a git repo
+	echo "Error, current repository is not a git repository."
+	exit 2
+fi
+
 mergeRepo $@
-moveRepoToDir $1
-pushAllChanges $1
-unhideFiles
