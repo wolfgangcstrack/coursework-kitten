2a2462a1955d8a7fa9d274652e20a36e3a50107a
Wolfgang C. Strack <vurufugangu@gmail.com>
2015-07-05 17:03:13 -0700

    Added a script for pulling all subtrees (untested)

---
+++ pull_subtrees.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Author: Wolfgang C. Strack
+# This script pulls all subtrees that were merged into the current git
+# repository using the merge_git_repo.bash script method of merging
+# subtrees.
+
+##### Initialize some stuff ##############################################
+usageMessage="Usage: $0 # no args"
+remoteNames=()
+
+##### Functions ##########################################################
+pullSubtrees ()
+{
+	echo
+	for remote in ${remoteNames[@]}; do
+		git pull -s subtree $remote master
+		echo
+	done
+}
+
+##### Main ###############################################################
+if [ $# -ne 0 ]; then
+	echo $usageMessage
+	exit 1
+fi
+
+remoteNames=($(git remote | grep -v origin))
+pullSubtrees
