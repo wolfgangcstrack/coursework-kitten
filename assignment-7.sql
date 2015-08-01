1c6ec9e5d561324f79dbcf7265092a883267d940
Wolfgang C. Strack <vurufugangu@gmail.com>
2015-07-31 21:10:47 -0700

    Start assignment 7

---
+++ assignment-7.sql
@@ -0,0 +1,23 @@
+-- Oracle SQL
+-- Lab Assignment 7
+-- Wolfgang C. Strack
+-- Student ID#: ****7355
+-- Due Date: 31 July 2015
+-- Date Handed In: 31 July 2015
+
+--------------------------------------------------
+
+DROP TABLE StudentFee CASCADE CONSTRAINTS;
+DROP TABLE Section CASCADE CONSTRAINTS;
+DROP TABLE Course CASCADE CONSTRAINTS;
+DROP TABLE Department CASCADE CONSTRAINTS;
+DROP TABLE User CASCADE CONSTRAINTS;
+DROP TABLE Resident CASCADE CONSTRAINTS;
+DROP TABLE FeeSchedule;
+DROP TABLE Location;
+DROP TABLE College;
+DROP TABLE Payment;
+
+-- Q1: Based on the schema in Lab 6, create tables with constraints.
+-- Consider using the following constraints as appropriate Primary Key,
+-- Foreign key, Unique, Null or Check.
