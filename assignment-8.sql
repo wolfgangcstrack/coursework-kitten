dd5972202c841cfa6e0acc9d43ec1c9f2b0a8324
Wolfgang C. Strack <vurufugangu@gmail.com>
2015-08-05 13:39:25 -0700

    Start Assignment 8

---
+++ assignment-8.sql
@@ -0,0 +1,25 @@
+-- Oracle SQL
+-- Lab Assignment 8
+-- Wolfgang C. Strack
+-- Student ID#: ****7355
+-- Due Date: 5 August 2015
+-- Date Handed In: 5 August 2015
+
+--------------------------------------------------
+
+-- 1. Setup a department, course within a department with 2 sections.
+-- Section must have a location assigned. Produce a report showing the
+-- department, its course and sections (with complete section information.)
+
+
+--------------------------------------------------
+
+-- 2. Now register a student to a section and process student payment.
+-- Produce a report showing student registration information, including
+-- payment information.
+
+
+--------------------------------------------------
+
+-- 3. How can you improve your DB Schema further. Make at least 2
+-- suggestions. 
