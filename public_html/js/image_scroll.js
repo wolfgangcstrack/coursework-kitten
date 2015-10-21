571bc30af892e3669f00974292f8755355e74715
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2015-10-20 18:17:55 -0700

    Ugly af but must save work and go to class

--- public_html/js/image_scroll.js
+++ public_html/js/image_scroll.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
-	$("#image_scroll img").each(function() {
-		// get old and new urls
-		var oldURL = $(this).attr("src");
-		var newURL = $(this).attr("id");
-		
-		// preload images		
-		var rolloverImage = new Image();
-		rolloverImage.src = newURL;
-		
-		// set up event handlers			
-		$(this).hover(
-			function() {
-				$(this).attr("src", newURL);
-			},
-			function() {
-				$(this).attr("src", oldURL);
-			}
-		); // end hover
-	}); // end each
-}); // end ready
+	var img_array = [];
+
+	$("#image_scroll img").each(function(index) {
+		img_array[index] = {};
+		img_array[index][$(this).attr("src")] = $(this).attr("id");
+	});
+
+	$("#right-scroll-button").click(function() {
+		$img1 = $("#image_scroll img:eq(0)");
+		$img2 = $("#image_scroll img:eq(1)");
+		$img3 = $("#image_scroll img:eq(2)");
+
+		var first_src = $img1.attr("src");
+		var first_id = $img1.attr("id");
+
+		$img1.attr("src", $img2.attr("src"));
+		$img1.attr("id",  $img2.attr("id"));
+		$img2.attr("src", $img3.attr("src"));
+		$img2.attr("id",  $img3.attr("id"));
+		$img3.attr("src", first_id);
+		$img3.attr("id",  first_src);
+	});
+});
