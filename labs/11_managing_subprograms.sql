cbc4899927524c78de6eec734d734341beddd289
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2015-11-03 21:50:45 -0800

    Start lab 9

---
+++ labs/11_managing_subprograms.sql
@@ -0,0 +1,20 @@
+-- Intro to PL/SQL
+-- Lab Assignment 9, Practice 11
+-- Wolfgang C. Strack
+-- Student ID#: ****7355
+-- Due Date: 2 November 2015
+-- Date Handed In: 3 November 2015
+
+-----
+
+----- Suppose you have lost the code for the NEW_EMP procedure and the
+----- VALID_DEPTID function that you created in lesson 10. (If you did not
+----- complete the practices in lesson 10, you can run the solution scripts to
+----- create the procedure and function.) Create an iSQL*Plus spool file to
+----- query the appropriate data dictionary view to regenerate the code.
+--- Hint:
+--- SET -- options ON|OFF
+--- SELECT -- statement(s) to extract the code
+--- SET -- reset options ON|OFF
+----- To spool the output of the file to a .sql file from iSQL*Plus, select the
+----- Save option for the Output and execute the code.
