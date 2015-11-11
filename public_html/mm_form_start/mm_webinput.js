bb5dd186a6b1c2de59bf7190db118bd9e1e4b58b
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2015-11-10 21:03:08 -0800

    Finish mm_webinput

--- public_html/mm_form_start/mm_webinput.js
+++ public_html/mm_form_start/mm_webinput.js
@@ -1,13 +1,39 @@
 $(document).ready(function() {
-	$("#first").focus();
-	
-	$("#mm_webinput").validate({
-		rules: {
-		},
-		messages: {
-			email_2: {
-				equalTo: "This entry must equal previous entry."
-			},
-		}
-	}); // end validate
+  $("#first").focus();
+  $("#mm_webinput").validate({
+    rules: {
+      first:      { required: true },
+      last:       { required: true },
+      "class":    { required: true },
+      semester:   { required: true },
+      year:       { required: true },
+      instructor: { required: true },
+      software:   { required: true },
+      ID_number: {
+        required: true,
+        digits: true,
+        rangelength: [6,6]
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      email_2: {
+        equalTo: "#email"
+      },
+      phone: {
+        required: true,
+        phoneUS: true
+      }
+    },
+
+    messages: {
+      email_2: {
+        equalTo: "The emails you have entered do not match."
+      },
+      ID_number: {
+        rangelength: "Please enter an ID of 6 digits"
+      }
+    }
+  }); // end validate
 }); // end ready
