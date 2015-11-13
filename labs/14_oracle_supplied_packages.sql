163e44f8389200e9f4983b60fbba160ab8cfc516
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2015-11-12 18:23:01 -0800

    Finish half of lab 12

--- labs/14_oracle_supplied_packages.sql
+++ labs/14_oracle_supplied_packages.sql
@@ -11,18 +11,41 @@
 --- a. Create a DROP_TABLE procedure that drops the table specified in the
 --- input parameter. Use the procedures and functions from the supplied
 --- DBMS_SQL package.
---& b. To test the DROP_TABLE procedure, first create a new table called
---- EMP_DUP as a copy of the EMPLOYEES table. 
---& c. Execute the DROP_TABLE procedure to drop the EMP_DUP table.
+CREATE OR REPLACE PROCEDURE drop_table(p_table_name IN VARCHAR2)
+IS
+  c_dbms_cursor INTEGER;
+  v_temp NUMBER;
+BEGIN
+  c_dbms_cursor := DBMS_SQL.OPEN_CURSOR;
+  DBMS_SQL.PARSE(c_dbms_cursor, 'DROP TABLE '||p_table_name, DBMS_SQL.NATIVE);
+  v_temp := DBMS_SQL.EXECUTE(c_dbms_cursor);
+  DBMS_SQL.CLOSE_CURSOR(c_dbms_cursor);
+END drop_table;
+/
 
+--- b. To test the DROP_TABLE procedure, first create a new table called
+--- EMP_DUP as a copy of the EMPLOYEES table. 
+CREATE TABLE emp_dup AS
+  SELECT * FROM employees;
+
+--- c. Execute the DROP_TABLE procedure to drop the EMP_DUP table.
+EXECUTE drop_table('emp_dup');
 
 -----
 
 ----- 2.
 --- a. Create another procedure called DROP_TABLE2 that drops the table
 --- specified in the input parameter. Use the EXECUTE IMMEDIATE statement.
---& b. Repeat the test outlined in steps 1-b and 1-c.
+CREATE OR REPLACE PROCEDURE drop_table2(p_table_name IN VARCHAR2) IS
+BEGIN
+  EXECUTE IMMEDIATE('DROP TABLE ' || p_table_name);
+END drop_table2;
 
+--- b. Repeat the test outlined in steps 1-b and 1-c.
+CREATE TABLE emp_dup AS
+  SELECT * FROM employees;
+
+EXECUTE drop_table('emp_dup');
 
 -----
 
@@ -30,10 +53,30 @@
 --- a. Create a procedure called ANALYZE_OBJECT that analyzes the given object
 --- that you specified in the input parameters. Use the DBMS_DDL package, and
 --- use the COMPUTE method.
---& b. Test the procedure using the EMPLOYEES table. Confirm that the
+CREATE OR REPLACE PROCEDURE analyze_object
+  (p_object_type IN VARCHAR2,
+   p_object_owner IN VARCHAR2,
+   p_object_name IN VARCHAR2)
+IS
+  v_last_analyzed_date DATE;
+BEGIN
+  DBMS_DDL.ANALYZE_OBJECT(p_object_type, p_object_owner, p_object_name, 'COMPUTE');
+
+  SELECT last_analyzed INTO v_last_analyzed_date
+  FROM user_tables WHERE table_name = UPPER('EMPLOYEES');
+
+  DBMS_OUTPUT.PUT_LINE(
+    'Last analysis date of ' || p_object_name ||
+    ' updated to: ' || v_last_analyzed_date
+  );
+END analyze_object;
+/
+
+--- b. Test the procedure using the EMPLOYEES table. Confirm that the
 --- ANALYZE_OBJECT procedure has run by querying the LAST_ANALYZED column in
 --- the USER_TABLES data dictionary view.
-
+SET SERVEROUTPUT ON;
+EXECUTE analyze_object('TABLE', 'sqluser35', 'employees');
 
 -----
 
