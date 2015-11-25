78ebcd0d8eb28374111d0ef782fd90e649289d24
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2015-11-24 23:45:05 -0800

    Start lab 15

---
+++ labs/17_more_trigger_concepts.sql
@@ -0,0 +1,20 @@
+-- Intro to PL/SQL
+-- Lab Assignment 15, Practice 17
+-- Wolfgang C. Strack
+-- Student ID#: ****7355
+-- Due Date: 24 November 2015
+-- Date Handed In: 24 November 2015
+
+-----
+
+----- A number of business rules that apply to the EMPLOYEES and DEPARTMENTS
+----- tables are listed below. Decide how to implement each of these business
+----- rules, by means of declarative constraints or by using triggers. Which
+----- constraints or triggers are needed and are there any problems to be
+----- expected? Implement the business rules by defining the triggers or
+----- constraints that you decided to create. A partial package is provided in
+----- file lab17_1.sql to which you should add any necessary procedures or
+----- functions that are to be called from triggers that you may create for the
+----- following rules. (The triggers should execute procedures or functions
+----- that you have defined in the package.)
+----- (See lesson notes for the rest of the assignment specification)
