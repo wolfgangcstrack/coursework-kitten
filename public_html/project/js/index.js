bd937a0021fa173bf751f094f1c3e71e0e186ca6
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2015-12-14 20:49:49 -0800

    Add common includes function and tabs/form jquery in index.js

--- public_html/project/js/index.js
+++ public_html/project/js/index.js
@@ -1,17 +1,47 @@
-$("document").ready(function() {
-  var navbar_links = '<a class="nav-link" href="#home-page">Home</a>' +
-                     '<a class="nav-link" href="#favorites-page">Favorites</a>' +
-                     '<a class="nav-link" href="#about-page">About</a>' +
-                     '<a class="nav-link" href="#contact-page">Contact</a>';
-  $('.aid-navbar').html(navbar_links);
-});
-$(document).ready(function() {
-  $("#cis89a-form").validate({
-    rules: {
-      first:        { required: true },
-      last:         { required: true },
-      email:        { required: true },
-      question:     { required: true }
+(function() {
+  function defer_jQuery(method) {
+    if (window.jQuery)
+       method(jQuery);
+    else
+      setTimeout(function() { defer_jQuery(method); }, 50);
+  }
+
+  function main($) {
+    insert_common_includes();
+
+    $(document).ready(function() {
+      $('#tabs').tabs();
+      $("#cis89a-form").validate({
+        rules: {
+          first:        { required: true },
+          last:         { required: true },
+          email:        { required: true },
+          question:     { required: true }
+        }
+      });
+    });
+  }
+
+  function insert_common_includes() {
+    var cssURLs = [
+    './css/theme.css',
+    './css/bootstrap-social.css',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
+    'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'
+    ];
+
+    function append_css(data) {
+      $('<style type="text/css">\n' + data + '</style>').appendTo("head");
     }
-  });
-});
+
+    for (var i = 0; i < cssURLs.length; i++) {
+      $.ajax({
+        url: cssURLs[i],
+        dataType: 'text',
+        success: append_css
+      });
+    }
+  }
+
+  defer_jQuery(main);
+})();
