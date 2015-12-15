b186c92e8645a482ed1f05fada9d8618a3ae51dd
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2015-12-14 21:10:45 -0800

    Add documentation to index.js

--- public_html/project/js/index.js
+++ public_html/project/js/index.js
@@ -1,27 +1,25 @@
 (function() {
-  function defer_jQuery(method) {
-    if (window.jQuery)
-       method(jQuery);
-    else
-      setTimeout(function() { defer_jQuery(method); }, 50);
-  }
-
+  /**
+   * All functions defined in this file besides defer_jQuery are called in
+   * this main function. Any jQuery/DOM manipulation functions are called
+   * within a $(document).ready callback function.
+   */
   function main($) {
+    // insert all common CSS stylesheets
     insert_common_includes();
 
+    // jQuery/DOM related functions called in here
     $(document).ready(function() {
-      $('#tabs').tabs();
-      $("#cis89a-form").validate({
-        rules: {
-          first:        { required: true },
-          last:         { required: true },
-          email:        { required: true },
-          question:     { required: true }
-        }
-      });
+      execute_jquery_ui();
+      execute_jquery_validation();
     });
   }
 
+  /**
+   * This function uses jQuery and AJAX to dynamically insert CSS
+   * stylesheets that can be considered common to a majority of the pages
+   * on the website.
+   */
   function insert_common_includes() {
     var cssURLs = [
     './css/theme.css',
@@ -41,6 +39,42 @@
         success: append_css
       });
     }
+  }
+
+  /**
+   * This function executes any related jQuery UI functionality for the
+   * page. Everything related to jQuery UI should be executed within here.
+   */
+  function execute_jquery_ui() {
+    $('#tabs').tabs();
+  }
+
+  /**
+   * This function executes any related jQuery validation functionality
+   * for the page. Everything related to jQuery validation should be
+   * executed within here.
+   */
+  function execute_jquery_validation() {
+    $("#cs-175-form").validate({
+      rules: {
+        first:        { required: true },
+        last:         { required: true },
+        email:        { required: true },
+        question:     { required: true }
+      }
+    });
+  }
+
+  /**
+   * This function is used to execute main ONLY after jQuery has been
+   * successfully loaded to the document; jQuery is included in its own
+   * script tag within the HTML document.
+   */
+  function defer_jQuery(method) {
+    if (window.jQuery)
+       method(jQuery);
+    else
+      setTimeout(function() { defer_jQuery(method); }, 50);
   }
 
   defer_jQuery(main);
